10e1e72cc05a9fb3145122f0c33c2ff635b09995
Glipotions <hamzakavak1905@gmail.com>
2022-08-15 18:53:05 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using Consul.Demo.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var consulHost = "http://localhost:8500";
+builder.Services.AddConsulConfig(configKey: consulHost);
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
